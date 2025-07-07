fa39fe85f704068267341f5a90eb80f84eeb72ab
Emil A. Cherrington <emil.cherrington@gmail.com>
2025-07-06 22:56:52 -0500

    Create README.js

---
+++ README.js
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* TITLE: HYPERSPECTRAL DATA TOOLKIT
+
+DESCRIPTION: This is a repository of Google Earth Engine (GEE) JavaScript code for tools for working with various hyperspectral
+open datasets from NASA (e.g., AVIRIS, EMIT, Hyperion, PACE OCI). One of the dependency packages included in this toolkit provides
+data on the hyperspectral wavelengths for NASA various spaceborne hyperspectral imagers, namely EO-1 Hyperion, the ISS EMIT, and 
+PACE OCI. Another dependency provides access to PACE OCI land data. Aside from the dependencies, example scripts are provided to allow
+users to interact with the data, and the geographic and temporal scopes of those examples can be modified to meet users' needs. The
+scripts are divided into the following main functions:
+
+(i) data discovery,
+(ii) data visualization,
+(iii) spectral signature visualization,
+(iv) data gap filling,
+(v) time series analysis, and
+(vi) image classification.
+
+Please note that this repository is a successor, as well as an add-on to the PACE OCI hyperspectral toolkit.
+
+AUTHOR: Emil A. Cherrington, Ph.D. (University of Alabama in Huntsville / NASA SERVIR); emil.cherrington@uah.edu
+
+CITATION: https://doi.org/10.5281/zenodo.15825295
+
+LAST UPDATED: 06.07.2025
+
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
